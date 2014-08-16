ad676bd16707da1fdfcc0d42446e8df82857d915
jonathan <JonDSchram@gmail.com>
2014-08-16 01:32:42 -0500

    Added code that was present before reset

---
+++ src/main/java/site/auth/LoginManager.java
@@ -0,0 +1,20 @@
+package site.auth;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class LoginManager implements HttpSessionListener {
+
+	@Override
+	public void sessionCreated(HttpSessionEvent se) {
+		// TODO Add user to HashSet of logged in users if desired
+		
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent se) {
+		// TODO Remove user from HashSet when implemented
+
+	}
+
+}
